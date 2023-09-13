d96caad550312217b06753bb7e8a473a2ee13b9c
Tianji Rao <rao@TTRao.local>
2023-09-13 15:19:14 -0400

    change files

--- mylib/lib.py
+++ mylib/lib.py
@@ -1,1 +1,29 @@
 import pandas as pd
+import polars as pl
+import matplotlib.pyplot as plt
+
+# For Pandas
+# describe
+def pd_desc(df):
+    return df.describe()
+
+# visualization: bar plot
+def pd_visual(df): 
+    df.plot(kind="hist")
+    plt.xticks(rotation = 90)
+    plt.xlabel('Index')
+    plt.ylabel(df.name)
+    plt.show()
+    
+    
+# For Polars
+# describe
+def polars_desc(df):
+    return df.describe()
+
+# visualization: histgram
+def polars_visual(df):
+    plt.hist(df["Model Year"])
+
+
+
