68e782ccff04b55cbd4a9b75fdc766c3f30fc2c5
Tianji Rao <rao@TTRao.local>
2023-09-18 13:07:50 -0400

    Update

--- script.py
+++ script.py
@@ -1,5 +1,7 @@
 from lib import pd_desc, mean, median, std
 
 def summary_desc(df):
-    # return pd_desc(df)
     return [mean(df), median(df), std(df)]
+
+def desc_pd(df):
+    return pd_desc(df)
--- test_script.py
+++ test_script.py
@@ -1,5 +1,17 @@
-from script import summary_desc
+from script import summary_desc, desc_pd
 
-# def test_summary_desc():
-    # 
+def test_summary_desc():
+    df = df.read_csv('Electric_Vehicle_Population_Data.csv')
+    
+    # mean
+    assert desc_pd(df).loc["mean", "Electric Range"] == 70.49573804284242
+    assert summary_desc(df["Electric Range"])[0] == 70.49573804284242
+    # median
+    assert desc_pd(df).loc["50%", "Model Year"] == 2021.0
+    assert summary_desc(df["Model Year"])[1] == 2021.0
 
+    # standard deviation
+    assert round(desc_pd(df).loc["std", "Electric Range"], 6) == 97.128735
+    assert round(summary_desc(df["Electric Range"])[2], 6) == 97.128735
+
+    
