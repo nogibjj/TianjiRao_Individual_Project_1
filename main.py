97144c3102415d8603ea38acf55d2bd897f5bbd1
TianoRao <104114843+TianoRao@users.noreply.github.com>
2023-09-20 01:45:48 +0000

    change

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from lib import pd_desc, mean, median, std
+from lib import pd_desc
+import pandas as pd
 
-def summary_desc(df):
-    return [mean(df), median(df), std(df)]
-
-def desc_pd(df):
+def summary_desc():
+    df = pd.read_csv('Electric_Vehicle_Population_Data.csv')
+    # return [mean(df), median(df), std(df)]
     return pd_desc(df)
--- test_main.py
+++ test_main.py
@@ -1,19 +1,16 @@
-from main import summary_desc, desc_pd
-import pandas as pd
+from main import summary_desc
+# import pandas as pd
 
 def test_summary_desc():
-    df = pd.read_csv('Electric_Vehicle_Population_Data.csv')
+    result = summary_desc()
     
     # mean
-    assert desc_pd(df).loc["mean", "Electric Range"] == 70.49573804284242
-    assert summary_desc(df["Electric Range"])[0] == 70.49573804284242
-    
+    assert result.loc["mean", "Electric Range"] == 70.49573804284242
+
     # median
-    assert desc_pd(df).loc["50%", "Model Year"] == 2021.0
-    assert summary_desc(df["Model Year"])[1] == 2021.0
+    assert result.loc["50%", "Model Year"] == 2021.0
 
     # standard deviation
-    assert round(desc_pd(df).loc["std", "Electric Range"], 6) == 97.128735
-    assert round(summary_desc(df["Electric Range"])[2], 6) == 97.128735
+    assert round(result.loc["std", "Electric Range"], 6) == 97.128735
 
     
