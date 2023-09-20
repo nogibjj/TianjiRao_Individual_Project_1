413e0cd3c8710a7b910c45ee3f77992cc7fc2a0d
Tianji Rao <rao@TTRao.local>
2023-09-19 21:06:45 -0400

    change test_lib.py

--- test_lib.py
+++ test_lib.py
@@ -2,7 +2,8 @@
 Test goes here
 """
 
-from lib import pd_desc
+from lib import pd_desc, mean, median, std
+
 import pandas as pd
 
 def test_desc_df():
@@ -13,7 +14,12 @@ def test_desc_df():
     df = pd.DataFrame(data)
     # mean
     assert pd_desc(df).loc["mean", "1"] == 2.0
+    assert mean(df['1']) == 2.0
+    
     # median
     assert pd_desc(df).loc["50%", "2"] == 4.0
+    assert median(df['2']) == 4.0
+    
     # standard deviation
     assert pd_desc(df).loc["std", "3"] == 1.0
+    assert std(df['3']) == 1.0
